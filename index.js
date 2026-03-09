02f2624a2abe37e6863d1ad723bd56eea373167d
PedroSuarez1402 <pssuarez1402@gmail.com>
2026-03-09 17:33:15 -0500

    configuracion de handlebars

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import router from "./routes/index.js";
+import { ExpressHandlebars } from "express-handlebars";
 
 const app = express();
+// Habilidar handlebars como motor de plantillas definiendo un layout
+const hbs = ExpressHandlebars.create({
+    defaultLayout: 'layout',
+});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
 const port = 3000;
+
+app.use('/', router);
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
